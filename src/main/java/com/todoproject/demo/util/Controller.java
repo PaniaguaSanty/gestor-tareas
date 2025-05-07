58d89a69a5bcd290103013d83f215b803e038035
Santiago Agustín Bosquet <114213431+PaniaguaSanty@users.noreply.github.com>
2025-05-07 13:39:21 -0300

    Generics for services and controllers added.

---
+++ src/main/java/com/todoproject/demo/util/Controller.java
@@ -0,0 +1,19 @@
+package com.todoproject.demo.util;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface Controller<R, Q> {
+
+    ResponseEntity<R> create(Q q);
+
+    ResponseEntity<List<R>> findAll();
+
+    ResponseEntity<R> findById(String id);
+
+    ResponseEntity<R> update(Q q);
+
+    ResponseEntity<Void> delete(String id);
+
+}
