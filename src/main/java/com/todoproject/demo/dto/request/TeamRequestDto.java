8212f5f496a91cb8c1dde23832eadb641bd7ad46
Santiago Agustín Bosquet <114213431+PaniaguaSanty@users.noreply.github.com>
2025-05-09 13:36:39 -0300

    Team model Updated

--- src/main/java/com/todoproject/demo/dto/request/TeamRequestDto.java
+++ src/main/java/com/todoproject/demo/dto/request/TeamRequestDto.java
@@ -12,5 +12,5 @@ import lombok.Setter;
 public class TeamRequestDto {
 
     private String name;
-
+    private boolean active;
 }
--- src/main/java/com/todoproject/demo/dto/response/TeamResponseDto.java
+++ src/main/java/com/todoproject/demo/dto/response/TeamResponseDto.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 public class TeamResponseDto {
 
     private String name;
+    private boolean active;
 }
